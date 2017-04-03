1ce5559179bb9ee1e8df08053dbb3f5253925b8d
Connor Kendrick <connorkendrickmusic@gmail.com>
2017-04-02 23:54:11 -0400

    Transfer map keys and values to a vector of pairs

--- frequency.cpp
+++ frequency.cpp
@@ -2,12 +2,20 @@
 #include <fstream>
 #include <string>
 #include <map>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
+
+bool sort_second(const pair<char,int> &a, const pair<char,int> &b)
+{
+  return (a.second < b.second);
+}
 
 int main()
 {
   map<char,int> freq_map;
+  vector< pair<char,int> > freq_vector;
 
   ifstream file;
   file.open("words.txt", ios::in);
@@ -30,8 +38,19 @@ int main()
     cout << "Unable to open file";
   }
 
+  for (map<char,int>::iterator mi = freq_map.begin(); mi != freq_map.end(); ++mi)
+  {
+    freq_vector.push_back(make_pair(mi->first, mi->second));
+  }
+
+  sort(freq_vector.begin(), freq_vector.end(), sort_second);
+
   cout << endl << endl;
-  cout << "Frequency of the letter 'b': " << freq_map['b'];
+
+  for (size_t i = 0; i < freq_vector.size(); ++i)
+  {
+    cout << freq_vector[i].first << ",\t" << freq_vector[i].second << endl;
+  }
 
   return 0;
 }
