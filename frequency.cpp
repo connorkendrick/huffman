0ef011997d206592847c31b07ffc0c4a0d84bf39
Connor Kendrick <connorkendrickmusic@gmail.com>
2017-04-02 22:01:27 -0400

    Read and print text file contents

--- frequency.cpp
+++ frequency.cpp
@@ -1,4 +1,26 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
 int main()
 {
+  string line;
+  ifstream file("words.txt");
+  if (file.is_open())
+  {
+    while (getline(file, line))
+    {
+      cout << line << '\n';
+    }
+
+    file.close();
+  }
+  else
+  {
+    cout << "Unable to open file";
+  }
+
   return 0;
 }
